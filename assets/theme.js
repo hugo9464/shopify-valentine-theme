d1cda3bc00f9f5f0eba96b44012c77b16d61d7d2
Hugo Faye <hugo.faye@gmail.com>
2021-10-12 11:01:42 +0200

    add product tabs

--- assets/theme.js
+++ assets/theme.js
@@ -9693,3 +9693,18 @@ function headerSize() {
 
 $(window).on("load", headerSize);
 $(window).on("resize", $.debounce(500, headerSize));
+
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent-cus");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks-cus");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("tab-cus-active").click();
